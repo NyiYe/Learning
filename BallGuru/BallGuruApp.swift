932dce122a4e612d96c404aa765ba8060f192958
Nyi Ye Han <nyiyehan@gmail.com>
2023-09-07 12:47:24 +0630

    Initial Commit

---
+++ BallGuru/BallGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallGuruApp.swift
+//  BallGuru
+//
+//  Created by Barry on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BallGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
